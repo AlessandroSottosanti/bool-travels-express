2fcce0c4f811b04cd206778d103af613b7a95903
AlessandroSottosanti <asottosanti2@gmail.com>
2025-02-01 11:48:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import notFoundPage from './middlewares/notFoundRoute.js';
+import handleError from "./middlewares/handleError.js";
+import cors from "cors";
+
+
+const app = express(); // Inizializza l'app Express
+
+const port = process.env.SERVER_PORT; // Porta su cui il server ascolterà
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+}))
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Definisci una rotta di base
+app.use("/travels", moviesRouter);
+
+
+
+app.use(notFoundPage.notFoundRoute);
+app.use(handleError);
+
+// Avvia il server
+app.listen(port, () => {
+    console.log(`Server in ascolto su http://localhost:${port}`);
+});
