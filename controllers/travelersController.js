a674c6daafc384163717548f83ea9dd322f76131
AlessandroSottosanti <asottosanti2@gmail.com>
2025-02-03 11:08:43 +0100

    refactoring: search

--- controllers/travelersController.js
+++ controllers/travelersController.js
@@ -1,73 +1,76 @@
 import connection from "../data/dbConnection.js";
 
 const index = (req, res, next) => {
-    const slug = req.params.slug;  
-    console.log("slug:", slug);
+    try {
+        const slug = req.params.slug;  
 
-    // query verifica esistenza viaggio
-    const sqlTravel = `
-        SELECT * FROM viaggi
-        WHERE viaggi.slug = ?
-    `;
+        // query verifica esistenza viaggio
+        const sqlTravel = `
+            SELECT * FROM viaggi
+            WHERE viaggi.slug = ?
+        `;
 
-    // Controlla che il viaggio con slug :slug esista
-    connection.query(sqlTravel, slug, (err, results) => {
-        if (err) {
-            return next(err);
-        } 
+        connection.query(sqlTravel, slug, (err, results) => {
+            if (err) {
+                console.error("Errore nella query del viaggio:", err);
+                return res.status(500).json({ status: "error", message: "Errore nel recupero del viaggio." });
+            } 
+            else if (results.length === 0) {
+                return res.status(404).json({
+                    status: "error",
+                    message: "Viaggio non trovato",
+                });
+            }
 
-        else if(results.length === 0) {
-            return res.status(404).json({
-                status: "error",
-                message: "Viaggio non trovato",
+            // Impostazione della query per i viaggiatori
+            let sql = `
+                SELECT Viaggiatori.*
+                FROM 
+                    Viaggiatori
+                INNER JOIN 
+                    Viaggi_Viaggiatori ON Viaggiatori.id = Viaggi_Viaggiatori.Viaggiatore_id
+                INNER JOIN 
+                    Viaggi ON Viaggi_Viaggiatori.viaggio_id = Viaggi.id
+                WHERE 
+                    Viaggi.slug = ?
+                
+            `;
+
+            const params = [slug];  // Parametro iniziale per lo slug
+
+            // Gestione del filtro di ricerca se presente
+            const filters = req.query;
+            console.log("filters:", filters.search);
+            if (filters.search) {
+                sql += " AND (Viaggiatori.nome LIKE ? OR Viaggiatori.cognome LIKE ?)";
+                params.push(`%${filters.search}%`);
+                params.push(`%${filters.search}%`);
+            }
+
+            // Esegui la query per ottenere i viaggiatori filtrati
+            connection.query(sql, params, (err, results) => {
+                if (err) {
+                    console.error("Errore nella query dei viaggiatori:", err);
+                    return res.status(500).json({ status: "error", message: "Errore nel recupero dei viaggiatori." });
+                } 
+                else if (results.length === 0) {
+                    return res.status(404).json({
+                        status: "error",
+                        message: "Nessun Viaggiatore trovato",
+                    });
+                } 
+                else {
+                    return res.status(200).json({
+                        status: "success",
+                        data: results,
+                    });
+                }
             });
-        }
-
-        return;
-    });
-
-
-    let sql = `SELECT Viaggiatori.*
-FROM 
-    Viaggiatori
-INNER JOIN 
-    Viaggi_Viaggiatori ON Viaggiatori.id = Viaggi_Viaggiatori.Viaggiatore_id
-INNER JOIN 
-    Viaggi ON Viaggi_Viaggiatori.viaggio_id = Viaggi.id
-WHERE 
-    Viaggi.slug = ?  -- Aggiungi lo slug come filtro
-ORDER BY 
-    Viaggi.destinazione, Viaggi.dataPartenza`;
-
-    const params = [slug];  // Passa lo slug come parametro alla query
-
-    // Gestione del filtro "search" (se presente nella query string)
-    const filters = req.query;
-    if (filters.search) {
-        sql += " AND (Viaggiatori.nome LIKE ? OR Viaggiatori.cognome LIKE ?)";
-        params.push(`%${filters.search}%`);
-        params.push(`%${filters.search}%`);
+        });
+    } catch (globalError) {
+        console.error("Errore generico nel controller:", globalError);
+        return res.status(500).json({ status: "error", message: "Errore interno del server." });
     }
-
-    connection.query(sql, params, (err, results) => {
-        if (err) {
-            return next(err);
-        } 
-        
-        else if(results.length === 0) {
-            return res.status(404).json({
-                status: "error",
-                message: "Nessun Viaggiatore trovato",
-            });
-        }
-
-        else {
-            return res.status(200).json({
-                status: "success",
-                data: results,
-            });
-        }
-    });
 };
 
 export default { index };
--- controllers/travelsController.js
+++ controllers/travelsController.js
@@ -1,57 +1,61 @@
 import connection from "../data/dbConnection.js";
 
 const index = (req, res, next) => {
-    
-    let sqlViaggi = `SELECT * FROM viaggi`;
+    try {
+        let sqlViaggi = `SELECT * FROM viaggi`;
 
-    connection.query(sqlViaggi, (err, viaggiResults) => {
-        if (err) {
-            return next(err);
-        }
-
-        let sqlGuide = `SELECT viaggi.id AS viaggio_id, guide.id AS guida_id, guide.nome AS nome_guida, guide.cognome AS cognome_guida
-                        FROM viaggi
-                        INNER JOIN viaggi_guide 
-                        ON viaggi.id = viaggi_guide.viaggio_id
-                        INNER JOIN guide 
-                        ON viaggi_guide.guida_id = guide.id`;
-
-        connection.query(sqlGuide, (err, guideResults) => {
+        connection.query(sqlViaggi, (err, viaggiResults) => {
             if (err) {
-                return next(err);
+                console.error("Errore nella query dei viaggi:", err);
+                return res.status(500).json({ status: "error", message: "Errore nel recupero dei viaggi." });
             }
 
-            let viaggiMap = {};
+            let sqlGuide = `SELECT viaggi.id AS viaggio_id, guide.id AS guida_id, guide.nome AS nome_guida, guide.cognome AS cognome_guida
+                            FROM viaggi
+                            INNER JOIN viaggi_guide 
+                            ON viaggi.id = viaggi_guide.viaggio_id
+                            INNER JOIN guide 
+                            ON viaggi_guide.guida_id = guide.id`;
 
-            viaggiResults.forEach(viaggio => {
-                viaggiMap[viaggio.id] = { 
-                    ...viaggio, 
-                    guide: []  
-                };
-            });
+            connection.query(sqlGuide, (err, guideResults) => {
+                if (err) {
+                    console.error("Errore nella query delle guide:", err);
+                    return res.status(500).json({ status: "error", message: "Errore nel recupero delle guide." });
+                }
 
-            // Inseriamo le guide nei rispettivi viaggi
-            guideResults.forEach(guida => {
-                const idViaggio = guida.viaggio_id;
+                try {
+                    let viaggiMap = {};
 
-                if (viaggiMap[idViaggio]) {
-                    viaggiMap[idViaggio].guide.push({
-                        id: guida.guida_id,
-                        nome: guida.nome_guida,
-                        cognome: guida.cognome_guida
+                    viaggiResults.forEach(viaggio => {
+                        viaggiMap[viaggio.id] = { 
+                            ...viaggio, 
+                            guide: []  
+                        };
                     });
+
+                    guideResults.forEach(guida => {
+                        const idViaggio = guida.viaggio_id;
+                        if (viaggiMap[idViaggio]) {
+                            viaggiMap[idViaggio].guide.push({
+                                id: guida.guida_id,
+                                nome: guida.nome_guida,
+                                cognome: guida.cognome_guida
+                            });
+                        }
+                    });
+
+                    const viaggiFinali = Object.values(viaggiMap);
+                    return res.status(200).json({ status: "success", data: viaggiFinali });
+                } catch (processingError) {
+                    console.error("Errore durante l'elaborazione dei dati:", processingError);
+                    return res.status(500).json({ status: "error", message: "Errore durante l'elaborazione dei dati." });
                 }
             });
-
-            // Convertiamo la mappa in un array di viaggi
-            const viaggiFinali = Object.values(viaggiMap);
-
-            return res.status(200).json({
-                status: "success",
-                data: viaggiFinali,
-            });
         });
-    });
+    } catch (globalError) {
+        console.error("Errore generico nel controller:", globalError);
+        return res.status(500).json({ status: "error", message: "Errore interno del server." });
+    }
 };
 
 export default { index };
